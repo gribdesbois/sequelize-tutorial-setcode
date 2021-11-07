c7a2c5be273ccd5fc730bc492e1626ded2ca9122
gribdesbois <gribdesbois@gmail.com>
2021-11-07 15:50:42 +0100

    db setup

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const app = express()
+const cors = require('cors')
+
+const auth = require('./authenticate')
+
+app.use(cors())
+// headers config
+app.use(express.json())
+app.options('*', cors()) // enables pre-flight requests before other routes
+auth()
+
+//! indique comment traiter les requetes vers la route /image
+/* app.use('/images', express.static(path.join(__dirname, 'images')))
+
+//! Express-rate-limit
+
+app.use('/api/sauces', apiLimiter, sauceRoutes) //! DO NOT FORGET / BEFORE api/sauces
+app.use('/api/auth', userRoutes)//! more limitations on route/user.js
+//! hit 429 if we hit this route too often */
+module.exports = app
---
+++ authenticate.js
@@ -0,0 +1,20 @@
+const Sequelize = require('sequelize');
+const dotenv = require('dotenv')
+
+dotenv.config()
+const {
+  DIALECT, DB_USER, WORD, TEST_DB, DB_URL,
+} = process.env
+
+const path = `${DIALECT}://${DB_USER}:${WORD}@${DB_URL}${TEST_DB}`;
+const sequelize = new Sequelize(path, { operatorsAliases: false });
+
+module.exports = () => {
+  sequelize.authenticate().then(() => {
+    console.log('Connection established successfully.');
+  }).catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  }).finally(() => {
+    sequelize.close();
+  });
+}
