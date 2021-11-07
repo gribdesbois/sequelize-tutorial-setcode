9b7a8c60d334bd861ae9f8518af45189bca5e322
gribdesbois <gribdesbois@gmail.com>
2021-11-07 22:31:19 +0100

    order-by

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 
 const { updateRow } = require('./basics/update_row')
 const { findAllRows } = require('./basics/find_all')
-const { getRows } = require('./basics/offset_limit')
+const { getRows } = require('./intermediate/order_by')
 
 app.use(cors())
 // headers config
---
+++ intermediate/order_by.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize')
+const dotenv = require('dotenv')
+
+dotenv.config()
+const {
+  DIALECT, DB_USER, WORD, TEST_DB, DB_URL,
+} = process.env
+
+const path = `${DIALECT}://${DB_USER}:${WORD}@${DB_URL}${TEST_DB}`
+const sequelize = new Sequelize(path, { operatorsAliases: false })
+
+const Note = sequelize.define('notes', {
+  description: Sequelize.STRING,
+})
+exports.getRows = async () => {
+  const notes = await Note.findAll({
+    order: [['description', 'DESC']],
+    attributes: ['id', 'description'],
+    raw: true,
+  })
+  console.log(notes)
+  sequelize.close()
+}
